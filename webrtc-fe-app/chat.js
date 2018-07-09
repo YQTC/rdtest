2ab0575093ce23fa3a95bc45180d5fa3b1f37652
qbing <q.bryant@live.com>
2018-07-09 11:25:20 +0800

    initialize

---
+++ webrtc-fe-app/chat.js
@@ -0,0 +1,389 @@
+var $chatlog, $input, $send, $name;
+
+// WebRTC objects
+var config = {
+  iceServers: [
+    { 
+      urls:           ["stun:139.199.180.239:7002", "turn:139.199.180.239:7002"], 
+      username:       'user',
+      credential:     'myPassword',
+      credentialType: 'password'
+    }
+    // { urls: ["stun:stun.l.google.com:19302"] }
+  ]
+}
+var cast = [
+  "Alice", "Bob", "Carol", "Dave", "Eve",
+  "Faythe", "Mallory", "Oscar", "Peggy",
+  "Sybil", "Trent", "Wendy"
+]
+
+var BOXID = "testbox123"
+
+window.PeerConnection = window.RTCPeerConnection ||
+                        window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
+window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
+
+var pc;  // PeerConnection
+var offer, answer;
+// Let's randomize initial username from the cast of characters, why not.
+var username = cast[Math.floor(cast.length * Math.random())];
+var channel;
+var myrandsessionid
+
+// Janky state machine
+var MODE = {
+  INIT:       0,
+  CONNECTING: 1,
+  CHAT:       2
+}
+var currentMode = MODE.INIT;
+
+// Signalling channel - just tells user to copy paste to the peer.
+var Signalling = {
+  send: function(msg) {
+    log("---- Please copy the below to peer ----\n");
+    log(JSON.stringify(msg));
+    log("\n");
+    sendLocalSDP(msg);
+  },
+  receive: function(msg) {
+    var recv;
+    try {
+      recv = JSON.parse(msg);
+    } catch(e) {
+      log("Invalid JSON.");
+      return;
+    }
+    if (!pc) {
+      start(false);
+    }
+    var desc = recv['sdp']
+    var ice = recv['candidate']
+    if (!desc && ! ice) {
+      log("Invalid SDP.");
+      return false;
+    }
+    if (desc) { receiveDescription(recv); }
+    if (ice) { receiveICE(recv); }
+  }
+}
+
+function welcome() {
+  log("== webrtc chat demo ==");
+  log("To initiate PeerConnection, type start. Otherwise, input SDP messages.");
+}
+
+function startChat() {
+  currentMode = MODE.CHAT;
+  $chatlog.className = "active";
+  log("------- chat enabled! -------");
+}
+
+function prepareDataChannel(channel) {
+  channel.onopen = function() {
+    log("Data channel opened!");
+    startChat();
+  }
+  channel.onclose = function() {
+    log("Data channel closed.");
+    currentMode = MODE.INIT;
+    $chatlog.className = "";
+    log("------- chat disabled -------");
+  }
+  channel.onerror = function() {
+    log("Data channel error!!");
+  }
+  channel.onmessage = function(msg) {
+    var recv = ab2str(msg.data);
+    console.log(msg);
+    var line = recv.trim();
+    log(line);
+  }
+}
+var decoder = new TextDecoder("utf-8");
+function ab2str(buf) {
+    return decoder.decode(new Uint8Array(buf));
+}
+
+function start(initiator) {
+  log("Starting up RTCPeerConnection...");
+  pc = new PeerConnection(config, {
+    optional: [
+      { DtlsSrtpKeyAgreement: true },
+      { RtpDataChannels: false },
+    ],
+  });
+  pc.onicecandidate = function(evt) {
+    console.log('onicecandidate.............');
+    var candidate = evt.candidate;
+    // Chrome sends a null candidate once the ICE gathering phase completes.
+    // In this case, it makes sense to send one copy-paste blob.
+    if (null == candidate) {
+      log("Finished gathering ICE candidates.");
+      Signalling.send(pc.localDescription);
+      return;
+    }
+  }
+  pc.onnegotiationneeded = function() {
+    console.log('onnegotiationneeded.............')
+    sendOffer();
+  }
+  pc.ondatachannel = function(dc) {
+    console.log('ondatachannel............');
+    channel = dc.channel;
+    log("Data Channel established... ");
+    prepareDataChannel(channel);
+  }
+
+  // Creating the first data channel triggers ICE negotiation.
+  if (initiator) {
+    channel = pc.createDataChannel("test");
+    prepareDataChannel(channel);
+  }
+}
+
+
+function sendOffer() {
+  var next = function(sdp) {
+    log("webrtc: Created Offer");
+    offer = sdp;
+    pc.setLocalDescription(sdp);
+    // sendLocalSDP(JSON.stringify(sdp));
+  }
+  var promise = pc.createOffer(next);
+  if (promise) {
+    promise.then(next);
+  }
+}
+
+function sendAnswer() {
+  var next = function (sdp) {
+    log("webrtc: Created Answer");
+    console.log("webrtc: Created Answer");
+    answer = sdp;
+    pc.setLocalDescription(sdp)
+
+    console.log(sdp);
+    sendLocalSDP(sdp);
+  }
+  console.log("即将应答！！");
+  try {
+    var promise = pc.createAnswer(next);
+    if (promise) {
+      console.log(12345)
+      promise.then(next);
+    }
+  } catch(e) {
+    console.log("创建应答失败");
+    console.log(e)
+  }
+}
+
+function receiveDescription(desc) {
+  var sdp = new RTCSessionDescription(desc);
+  // try {
+  //   err = pc.setRemoteDescription(sdp);
+  // } catch (e) {
+  //   log("Invalid SDP message.");
+  //   return false; 
+  // }
+  // console.log("Set remote ret:", err);
+  // log("SDP " + sdp.type + " successfully received.");
+  // if ("offer" == sdp.type) {
+  //   sendAnswer();
+  // }
+  pc.setRemoteDescription(sdp)
+  .then(()=>{
+    console.log("Set remote ret:");
+    log("SDP " + sdp.type + " successfully received.");
+    if ("offer" == sdp.type) {
+      sendAnswer();
+    }    
+  })
+  return true;
+}
+
+function receiveICE(ice) {
+  var candidate = new RTCIceCandidate(ice);
+  try {
+    pc.addIceCandidate(candidate);
+  } catch (e) {
+    log("Could not add ICE candidate.");
+    return;
+  }
+  log("ICE candidate successfully received: " + ice.candidate);
+}
+
+function waitForSignals() {
+  currentMode = MODE.CONNECTING;
+}
+
+function ajax(obj){
+    // 默认参数
+    var defaults = {
+        type : 'get',
+        data : {},
+        url : '#',
+        dataType : 'text',
+        async : true,
+        success : function(data){console.log(data)}
+    }
+    // 处理形参，传递参数的时候就覆盖默认参数，不传递就使用默认参数
+    for(var key in obj){//把输入的参数与设置的默认数据进行覆盖更新
+        defaults[key] = obj[key];
+    }
+    // 1、创建XMLHttpRequest对象
+    var xhr = null;
+    if(window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+    }else{
+        xhr = new ActiveXObject('Microsoft.XMLHTTP');// 兼容ie的早期版本
+    }
+    // 把对象形式的参数转化为字符串形式的参数
+    /* {username:'zhangsan','password':123} 转换为 username=zhangsan&password=123 */
+    var param = '';
+    for(var attr in obj.data){
+        param += attr + '=' + obj.data[attr] + '&';
+    }
+    if(param){//substring(start, end)截取字符串去掉最后的&符号
+        param = param.substring(0,param.length - 1);
+    }
+    // 处理get请求参数并且处理中文乱码问题
+    if(defaults.type == 'get'){
+        defaults.url += '?' + encodeURI(param);
+    }
+    // 2、准备发送（设置发送的参数）
+    xhr.open(defaults.type,defaults.url,defaults.async); // 处理post请求参数并且设置请求头信息（必须设置）
+    var data = null;
+    if(defaults.type == 'post'){
+        data = param;
+        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    //post模式下必须加的请求头，这个请求头是告诉服务器怎么去解析请求的正文部分。
+    }
+    // 3、执行发送动作
+    xhr.send(data);
+    // 处理同步请求，不会调用回调函数
+    if(!defaults.async){
+        if(defaults.dataType == 'json'){
+            return JSON.parse(xhr.responseText);
+        }else{
+            return xhr.responseText;
+        }
+    }
+    // 4、指定回调函数（处理服务器响应数据）
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            //4 获取数据成功
+        if(xhr.status == 200){
+            //200 获取的数据格式正确
+            var data = xhr.responseText;
+            if(defaults.dataType == 'json'){
+                // data = eval("("+ data +")");
+                data = JSON.parse(data);
+                //JSON.parse把获取带的json格式的数据转化为js的对象形式可以使用
+                }
+                defaults.success(data);//回调函数
+            }
+        }
+    }
+}
+
+function setRemoteSDP(sdp) {
+  Signalling.receive(sdp);
+}
+
+function getRemoteBoxSDP() {
+  return new Promise(function(resolve,reject){
+    var internalGetBoxSdp = function(){
+      ajax({
+        type: "post",
+        url: "https://www.yqtc.co/iamtest/ubbey/turn/app_connect",
+        data: {
+          box_id: BOXID,
+          action: 0
+        },
+        dataType: "json",
+        success: function(res) {
+          log("internalGetAppSdp return:" + JSON.stringify(res));
+          if(res.err_no === 0 && res.box_sdp) {
+            resolve(res)
+          } else {
+            // alert(res.err_msg || "获取盒子sdp错误")
+            log("获取远程盒子错误, 稍后自动改重试");
+            setTimeout(internalGetBoxSdp, 5000);
+          }
+        }
+      })  
+    }
+    setTimeout(internalGetBoxSdp, 0);
+  })
+}
+
+function sendLocalSDP(sdp) {
+  return new Promise(function(resolve,reject){
+    setTimeout(()=>{
+      send4sdp = {}
+      send4sdp.type = sdp.type
+      send4sdp.sdp = sdp.sdp
+      send4sdp.myrandsessionid = myrandsessionid
+      ajax({
+        type: "post",
+        url: "https://www.yqtc.co/iamtest/ubbey/turn/app_connect",
+        data: {
+          box_id: BOXID,
+          action: 1,
+          app_sdp: JSON.stringify(send4sdp)
+        },
+        dataType: "json",
+        success: function(res) {
+          console.log(res);
+          if(res.err_no === 0) {
+            log("上传本地sdp成功:" + JSON.stringify(send4sdp));
+          } else {
+            //alert(res.err_msg || "app上传sdp错误")
+          }
+        }
+      })
+    }, 0) 
+  })
+}
+
+// Get DOM elements and setup interactions.
+function init() {
+  console.log("loaded");
+  // Setup chatwindow.
+  $chatlog = document.getElementById('chatlog');
+  $chatlog.value = "";
+
+  welcome();
+
+  getRemoteBoxSDP()
+    .then((res)=>{
+      log("successfully got box_sdp:" + res.box_sdp + "\n")
+      log("should start to create answer & send app_sdp\n")
+      box_sdp = JSON.parse(res.box_sdp)
+      myrandsessionid = box_sdp.myrandsessionid || getSdpId(box_sdp.sdp)
+      Signalling.receive(res.box_sdp)
+    }); 
+  // start(true);
+}
+
+function getSdpId(sdp){
+  return sdp.split("\r\n")[1].split(" ")[1]
+}
+
+function speak(msg) {
+  log(msg);
+  channel.send(msg);
+}
+
+var log = function(msg) {
+  $chatlog.value += msg + "\n";
+  console.log(msg);
+  // Scroll to latest.
+  $chatlog.scrollTop = $chatlog.scrollHeight;
+}
+
+window.onload = init;
---
+++ webrtcbox/server/server.go
@@ -0,0 +1,294 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/keroserene/go-webrtc"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type SdpReq struct {
+	Box_id  string `json:"box_id"`
+	Action  int    `json:"action"`
+	Box_sdp string `json:"box_sdp"`
+}
+
+type SdpRsp struct {
+	Err_no  int    `json:"err_no"`
+	Err_msg string `json:"err_msg"`
+	App_sdp string `json:"app_sdp"`
+}
+
+const BOXID = "testbox123"
+func mainprocess() {
+	var (
+		ChOnGenerateOffer = make(chan int, 1)
+		ChSignalRegister  = make(chan string, 1)
+		ChStartGetAppSdp  = make(chan string, 1)
+		ChStartSetBoxSdp  = make(chan string, 1)
+		ChAllOk = make(chan int, 1)
+	)
+
+	// Step 1. create pc
+	pc := createpc()
+
+	// Step 2. register callback
+	registerCallback(pc, ChOnGenerateOffer, ChSignalRegister)
+
+	// Step 3. createoffer
+	go func() {
+		<-ChOnGenerateOffer //wait
+		generateOffer(pc)
+	}()
+
+	// Step 4. registerBoxSdp
+	go func() {
+		sdp := <-ChSignalRegister //wait
+		registerBoxSdp(sdp)
+		ChStartGetAppSdp <- sdp
+	}()
+
+	// Step 5. getRemoteAppSdp
+	go func() {
+		box_sdp := <-ChStartGetAppSdp //wait
+		app_sdp := getRemoteAppSdpUtilSuccess(box_sdp)
+		ChStartSetBoxSdp <- app_sdp
+	}()
+
+	// Step 6. setBoxLocalRemoteSdp
+	go func() {
+		app_sdp := <-ChStartSetBoxSdp //wait
+		setBoxLocalRemoteSdp(app_sdp, pc)
+		ChAllOk<-1
+	}()
+
+	// Step 7. blocked & loop & print status info
+	var endchat bool = false
+	dc := prepareDataChannel(pc, &endchat)
+	time.Sleep(5 * time.Second)
+	fmt.Printf("====Waiting all ok===\n", )
+	<-ChAllOk
+	for !endchat{
+		msg := "i am server\n"
+		fmt.Printf("server send data : %s\n", msg)
+		fmt.Printf("DataChannel state : %s\n", dc.ReadyState().String())
+		dc.Send([]byte(msg))
+		time.Sleep(5 * time.Second)
+	}
+}
+func main(){
+	for{
+		fmt.Println("!!!Start a new session!!!!")
+		mainprocess()
+	}
+}
+
+func createpc() *webrtc.PeerConnection {
+	fmt.Println("Initbox...")
+	fmt.Println("Starting up PeerConnection config...")
+	urls := []string{"turn:139.199.180.239:7002", "stun:139.199.180.239:7002"}
+	s := webrtc.IceServer{Urls: urls, Username: "admin", Credential: "admin"} //Credential:"turn.yqtc.top"
+	webrtc.NewIceServer()
+	config := webrtc.NewConfiguration()
+	config.IceServers = append(config.IceServers, s)
+
+	pc, err := webrtc.NewPeerConnection(config)
+	if nil != err {
+		fmt.Println("Failed to create PeerConnection.")
+		return pc
+	}
+	return pc
+}
+
+func registerCallback(pc *webrtc.PeerConnection, ChCanGenOffer chan int, ChCanRegisterSdp chan string) {
+	// OnNegotiationNeeded is triggered when something important has occurred in
+	// the state of PeerConnection (such as creating a new data channel), in which
+	// case a new SDP offer must be prepared and sent to the remote peer.
+	pc.OnNegotiationNeeded = func() {
+		ChCanGenOffer <- 1
+	}
+
+	// Once all ICE candidates are prepared, they need to be sent to the remote
+	// peer which will attempt reaching the local peer through NATs.
+	pc.OnIceComplete = func() {
+		fmt.Println("Finished gathering ICE candidates.")
+		sdp := pc.LocalDescription().Serialize()
+		ChCanRegisterSdp <- sdp
+	}
+}
+
+func generateOffer(pc *webrtc.PeerConnection) {
+	fmt.Println("Generating offer...")
+	offer, err := pc.CreateOffer() // blocking
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	pc.SetLocalDescription(offer)
+}
+
+func registerBoxSdp(msg string) {
+	fmt.Println(" ---- register sdp to host ---- ")
+	url := "http://iamtest.yqtc.co/ubbey/turn/box_sdp"
+	body := SdpReq{}
+
+	body.Box_id = BOXID
+	body.Action = 0
+	body.Box_sdp = msg
+
+	b, _ := json.Marshal(body)
+
+	req, _ := http.NewRequest("POST", url, bytes.NewReader(b))
+	req.Header.Add("Content-type", "application/json")
+	cli := http.Client{}
+
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+	cli.Transport = tr
+
+	r, err := cli.Do(req)
+	if err != nil {
+		fmt.Printf("http err :%s\n", err.Error())
+	}
+
+	rspb, _ := ioutil.ReadAll(r.Body)
+
+	fmt.Printf("http rsp :%s\n", rspb)
+
+}
+
+func getRemoteAppSdp() (sdp string, boxsid string, err error) {
+	fmt.Println(" ---- get sdp connect from host ---- ")
+
+	body := SdpReq{}
+	body.Box_id = BOXID
+	body.Action = 1
+
+	url := "http://iamtest.yqtc.co/ubbey/turn/box_sdp"
+
+	b, _ := json.Marshal(body)
+
+	req, _ := http.NewRequest("POST", url, bytes.NewReader(b))
+	req.Header.Add("Content-type", "application/json")
+
+	cli := http.Client{}
+
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+	cli.Transport = tr
+
+	r, err := cli.Do(req)
+	if err != nil {
+		fmt.Printf("http err :%s\n", err.Error())
+		return "", "", err
+	}
+
+	rspb, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("sdp http success :%s\n", rspb)
+	rsp := SdpRsp{}
+	json.Unmarshal(rspb, &rsp)
+
+	fmt.Printf("get sdp rsp %+v\n", rsp)
+	if rsp.App_sdp == "" {
+		return "", "", errors.New("rsp.App_sdp is empty")
+	}
+
+	var objmap map[string]*json.RawMessage
+	err = json.Unmarshal([]byte(rsp.App_sdp), &objmap)
+	if err != nil {
+		fmt.Printf("Unmarshal App_sdp error:%s\n", err.Error())
+		return "", "", err
+	} else if _, ok := objmap["myrandsessionid"]; ok && nil == json.Unmarshal(*objmap["myrandsessionid"], &boxsid) {
+		sdp = rsp.App_sdp
+		return sdp, boxsid, nil
+	} else {
+		return "", "", errors.New("rsp.App_sdp have no 'myrandsessionid' field")
+	}
+}
+
+func getRemoteAppSdpUtilSuccess(box_sdp string) (appsdp string) {
+	for {
+		app_sdp, box_sid, err := getRemoteAppSdp()
+		if err == nil && strings.Index(box_sdp, box_sid) >= 0 {
+			fmt.Printf("====Success get app_sdp:%s, ===box_sdp:%s, ===box_sid:%s\n",app_sdp, box_sdp, box_sid)
+			fmt.Println("====Success get app_sdp, ready to setBoxLocalRemoteSdp")
+			appsdp = app_sdp
+			break
+		} else if err == nil {
+			fmt.Printf("====Success get app_sdp:%s, ===box_sdp:%s, ===box_sid:%s\n",app_sdp, box_sdp, box_sid)
+			fmt.Println("====Success get app_sdp, but box_sid NOT match, retry....")
+			time.Sleep(5 * time.Second)
+			continue
+		} else {
+			fmt.Println("====Failed get app_sdp, retry, error=" + err.Error())
+			time.Sleep(5 * time.Second)
+			continue
+		}
+	}
+	return
+}
+
+func setBoxLocalRemoteSdp(msg string, pc *webrtc.PeerConnection) {
+	var parsed map[string]interface{}
+	err := json.Unmarshal([]byte(msg), &parsed)
+	if nil != err {
+		fmt.Println(err, ", try again.")
+		fmt.Println("input msg=" + msg)
+		return
+	}
+
+	if nil != parsed["sdp"] {
+		sdp := webrtc.DeserializeSessionDescription(msg)
+		if nil == sdp {
+			fmt.Println("Invalid SDP.")
+			return
+		}
+
+		err = pc.SetRemoteDescription(sdp)
+		if nil != err {
+			fmt.Println("ERROR", err)
+			return
+		}
+		fmt.Println("SDP " + sdp.Type + " successfully received.")
+	}
+
+	// Allow individual ICE candidate messages, but this won't be necessary if
+	// the remote peer also doesn't use trickle ICE.
+	if nil != parsed["candidate"] {
+		ice := webrtc.DeserializeIceCandidate(msg)
+		if nil == ice {
+			fmt.Println("Invalid ICE candidate.")
+			return
+		}
+		pc.AddIceCandidate(*ice)
+		fmt.Println("ICE candidate successfully received.")
+	}
+	fmt.Println("\nNormal exit setBoxLocalRemoteSdp")
+}
+
+func prepareDataChannel(pc *webrtc.PeerConnection, endchat *bool) (dc *webrtc.DataChannel) {
+	// Attempting to create the first datachannel triggers ICE.
+	fmt.Println("prepareDataChannel datachannel....")
+	dc, err := pc.CreateDataChannel("test")
+	if nil != err {
+		fmt.Println("Unexpected failure creating Channel.")
+		return
+	}
+
+	dc.OnOpen = func() {
+		fmt.Println("Data Channel Opened!")
+		//startChat()
+	}
+	dc.OnClose = func() {
+		fmt.Println("Data Channel closed.")
+		*endchat = true
+	}
+	dc.OnMessage = func(msg []byte) {
+		fmt.Printf("recv msg : %s\n", msg)
+	}
+	return dc
+}
